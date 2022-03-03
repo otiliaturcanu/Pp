6c2c97d570cfe9342530b6aff81c9194ee21ae0d
otiliaturcanu <otilia-ariadna.turcanu@student.tuiasi.ro>
2022-03-04 00:10:53 +0200

    Problema2 Eliminare spatii multiple

--- tema1/Problema2/main.py
+++ tema1/Problema2/main.py
@@ -6,6 +6,15 @@ def eliminare_semne_punctuatie(file):
         print(x)
 
 
+def eliminare_spatii_multiple(file):
+    for line in file:
+        x = re.sub(r"\s+", " ", line)
+        print(x)
+
+
 f = open("input.txt", "r")
 print("ELIMINARE SEMNE DE PUNCTUATIE:")
 eliminare_semne_punctuatie(f)
+print("ELIMINARE SPATII MULTIPLE:")
+f.seek(0)
+eliminare_spatii_multiple(f)
