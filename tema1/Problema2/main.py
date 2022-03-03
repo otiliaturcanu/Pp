afb6fe1406905fd22f75e2fba63a02c63f3e239e
otiliaturcanu <otilia-ariadna.turcanu@student.tuiasi.ro>
2022-03-04 00:12:40 +0200

    Problema2 Filtrare cuvinte dupa lungime

--- tema1/Problema2/main.py
+++ tema1/Problema2/main.py
@@ -12,9 +12,26 @@ def eliminare_spatii_multiple(file):
         print(x)
 
 
+def filtrare_nr_litere(file, nr):
+    found = []
+    for line in file:
+        pattern = re.compile(r"\b\w{"+str(nr)+r"}\b")
+        x = pattern.findall(line)
+        for i in x:
+            found.append(i)
+    if len(found):
+        print(f"Cuvintele cu {nr} litere din. inputul dat sunt:", end=" ")
+        print(found)
+    else:
+        print(f"Nu exista cuvinte de lungime {nr} in inputul dat.")
+
+
 f = open("input.txt", "r")
 print("ELIMINARE SEMNE DE PUNCTUATIE:")
 eliminare_semne_punctuatie(f)
 print("ELIMINARE SPATII MULTIPLE:")
 f.seek(0)
 eliminare_spatii_multiple(f)
+print("FILTRARE CUVINTE CU UN ANUMIT NUMAR DE LITERE")
+f.seek(0)
+filtrare_nr_litere(f, 4)
